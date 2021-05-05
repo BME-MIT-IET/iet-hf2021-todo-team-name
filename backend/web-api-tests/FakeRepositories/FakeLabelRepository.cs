ed7f521139869858e8cee609097c29128f44b997
Patrik Ángyán <58144458+MrPatesz@users.noreply.github.com>
2021-05-05 15:36:07 +0200

    created FakeLabelRepository & 1st test

---
+++ backend/web-api-tests/FakeRepositories/FakeLabelRepository.cs
@@ -0,0 +1,45 @@
+﻿using Deadline.DB.IRepositories;
+using Deadline.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web_api_tests.FakeRepositories
+{
+    class FakeLabelRepository : ILabelRepository
+    {
+        private readonly List<Label> labels;
+
+        public FakeLabelRepository()
+        {
+            labels = new List<Label>()
+            {
+                new Label() { ID = "0", name = "iet_name", type = "iet_type", userid = "0" },
+                new Label() { ID = "1", name = "test_name", type = "test_type", userid = "0" },
+                new Label() { ID = "2", name = "test_name2", type = "test_type2", userid = "1" },
+            };
+        }
+
+        public async Task<Label> AddLabel(Label label, string userid)
+        {
+            label.ID = Guid.NewGuid().ToString();
+            label.userid = userid;
+            labels.Add(label);
+            return label;
+        }
+
+        public async Task<string> Delete(string labelid, string userid)
+        {
+            var existing = labels.First(l => l.ID == labelid && l.userid == userid);
+            labels.Remove(existing);
+
+            return labelid;
+        }
+
+        public async Task<List<Label>> GetLabels(string userid)
+        {
+            return labels.FindAll(l => l.userid == userid).ToList();
+        }
+    }
+}
---
+++ backend/web-api-tests/LabelControllerTest.cs
@@ -0,0 +1,38 @@
+﻿using Deadline.Controllers;
+using Deadline.DB.IRepositories;
+using Deadline.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using web_api_tests.FakeRepositories;
+using Xunit;
+
+namespace web_api_tests
+{
+    public class LabelControllerTest
+    {
+        LabelController controller;
+        ILabelRepository repository;
+
+        public LabelControllerTest()
+        {
+            //Arrange
+            repository = new FakeLabelRepository();
+            controller = new LabelController(repository);
+
+            controller.ControllerContext = new ControllerContext();
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            controller.ControllerContext.HttpContext.Items.Add("UserID", "0");
+        }
+
+        [Fact]
+        public void Get_WhenCalled_ReturnsAllItems()
+        {
+            // Act
+            var result = controller.GetLabels().Result;
+            // Assert
+            var items = Assert.IsType<List<Label>>(result.Value);
+            Assert.Equal(2, items.Count);
+        }
+    }
+}
